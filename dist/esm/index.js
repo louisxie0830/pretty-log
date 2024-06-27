c323ad45ecb7f8cb134aa1b9fe34a4a88e8f497b
Nil Xie <bfgh2678@gmail.com>
2024-06-28 01:16:29 +0800

    update dist

--- dist/esm/index.js
+++
@@ -1,5 +0,0 @@
-import prettyLog from './prettyLog';
-const createPrettyLog = (config) => {
-    return prettyLog(config);
-};
-export default createPrettyLog;
--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import prettyLog, { PrettyLogConfig } from './prettyLog';
-type PrettyLogger = ReturnType<typeof prettyLog>;
-declare const createPrettyLog: (config: PrettyLogConfig) => PrettyLogger;
-export default createPrettyLog;
